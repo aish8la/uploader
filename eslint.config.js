7c3364c40001828e0b4eeaf1ae79b2c469e34587
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-12-08 21:50:46 +0500

    build: setup project with dev tools

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": [
+        "error",
+        {
+          allowInterfaces: "with-single-extends",
+        },
+      ],
+    },
+  },
+]);
