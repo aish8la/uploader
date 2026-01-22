e2a170a25191f4c3cabd65555c9e09016db4544c
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2026-01-23 00:52:15 +0500

    build: setup EJS

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
