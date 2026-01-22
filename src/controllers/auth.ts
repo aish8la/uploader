f460d54f06ab7d20191fca3b6506a1a95c624e79
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2026-01-23 02:40:48 +0500

    feat: add login and logout function

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { NewUser } from "../schemas/user.schema.js";
 import { postNewUser } from "../services/userService.js";
 import * as argon2 from "argon2";
+import passport from "passport";
 
 export const getLogin: express.RequestHandler = (req, res) => {
   res.render("auth/login");
@@ -22,4 +23,29 @@ export const postSignup: express.RequestHandler = async (req, res) => {
 
   await postNewUser(data);
   res.redirect("/");
+};
+
+export const postLogin: express.RequestHandler = (req, res, next) => {
+  const authCallback: passport.AuthenticateCallback = (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.render("auth/login", { errors: info });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      req.session.save((err) => {
+        if (err) return next(err);
+        return res.redirect("/");
+      });
+    });
+  };
+  passport.authenticate("local", authCallback)(req, res, next);
+};
+
+export const getLogout: express.RequestHandler = (req, res, next) => {
+  req.logOut((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
 };
--- src/routes/routes/auth.ts
+++ src/routes/routes/auth.ts
@@ -2,10 +2,16 @@ import express from "express";
 import * as authControllers from "../../controllers/auth.js";
 const router: express.Router = express.Router();
 
-router.route("/login").get(authControllers.getLogin);
+router
+  .route("/login")
+  .get(authControllers.getLogin)
+  .post(authControllers.postLogin);
+
 router
   .route("/signup")
   .get(authControllers.getSignup)
   .post(authControllers.postSignup);
+
+router.route("/logout").get(authControllers.getLogout);
 
 export default router;
