3c25e7274be1fe220e364a3fc2413e079ed8aa3b
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2026-03-05 03:29:01 +0500

    feat: implement function to create folder in file system

---
+++ src/services/fileService.ts
@@ -0,0 +1,52 @@
+import path from "path";
+import fs from "fs/promises";
+import * as storage from "../configs/storage.js";
+import type { User } from "../schemas/user.schema.js";
+import { prisma } from "../configs/prisma.js";
+
+const createFsDirectory = async (fullPath: string) => {
+  try {
+    await fs.mkdir(path.join(storage.STORAGE_ROOT, fullPath), {
+      recursive: true,
+    });
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const fsPathBuilder = (
+  userId: User["id"],
+  folderId: string,
+  parentId?: string,
+) => {
+  return path.join(userId.toString(), parentId ? parentId : ".", folderId);
+};
+
+export const saveDirectory = async (
+  userId: User["id"],
+  folderName: string,
+  parentId?: string,
+) => {
+  try {
+    const folder = await prisma.folder.create({
+      data: {
+        name: folderName,
+        ownerId: userId,
+        parentId: parentId ? parentId : null,
+      },
+    });
+
+    if (!folder) return false;
+
+    const fsPath = fsPathBuilder(userId, folder.id, parentId);
+
+    const success = await createFsDirectory(fsPath);
+
+    if (!success) return false;
+
+    return folder;
+  } catch {
+    return false;
+  }
+};
