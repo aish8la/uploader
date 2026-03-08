92877ddbbb482a2ca408c8bb34313bd2255666c2
aish <64914670+aish8la@users.noreply.github.com>
2026-03-08 12:49:47 +0500

    feat: remove redundant column for file meta data

---
+++ prisma/migrations/20260308074915_remove_storage_key_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `storage_key` on the `File` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "storage_key";
