21c4296f071d79991b5cc152323bf40c915601fb
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2026-03-11 21:49:34 +0500

    feat: added multer config to upload files

--- src/configs/multer.ts
+++ src/configs/multer.ts
@@ -1,12 +1,45 @@
 import multer from "multer";
+import * as fs from "fs/promises";
+import * as path from "path";
+import * as storagePath from "../configs/storage.js";
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
+  destination: async (req, file, cb) => {
+    if (!req.user?.id) {
+      return cb(new Error("User needs to be logged in"), "");
+    }
+
+    const filePath = path.join(
+      storagePath.STORAGE_ROOT,
+      req.user.id.toString(),
+    );
+
+    try {
+      await fs.mkdir(filePath, { recursive: true });
+    } catch (error) {
+      return cb(error as Error, "");
+    }
+
+    cb(null, filePath);
+  },
   filename: (req, file, cb) => {
-    const name = file.originalname;
-    console.log(req);
+    if (!req.user?.id) {
+      return cb(new Error("User needs to be logged in"), "");
+    }
+
+    const name =
+      req.user.id.toString() +
+      "_" +
+      Date.now().toString() +
+      path.extname(file.originalname);
     cb(null, name);
   },
 });
 
-export const upload = multer({ storage: storage });
+export const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 3 * 1024 * 1024,
+    files: 5,
+  },
+});
