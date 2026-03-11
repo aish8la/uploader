008c30eda9c1293a8cb3db1859afc6d09cd11b15
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2026-03-11 16:28:13 +0500

    fix: fixed storage path

--- src/configs/storage.ts
+++ src/configs/storage.ts
@@ -2,5 +2,5 @@ import { fileURLToPath } from "url";
 import path from "path";
 
 export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
-export const STORAGE_ROOT = path.join(__dirname, "../../uploads");
+export const __dirname = path.dirname(path.join(__filename, ".."));
+export const STORAGE_ROOT = path.join(__dirname, "../uploads");
