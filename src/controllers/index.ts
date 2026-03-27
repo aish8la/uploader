9a753f3722134c68196c06ab3934a09afd7cefdb
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2026-03-27 20:01:48 +0500

    fix: fixed redirect url after auth

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 export const getRoot: express.RequestHandler = (req, res) => {
   const isAuth = req.isAuthenticated();
   if (isAuth) {
-    return res.redirect("my-drive");
+    return res.redirect("/my-drive");
   }
   res.redirect("auth/login");
 };
